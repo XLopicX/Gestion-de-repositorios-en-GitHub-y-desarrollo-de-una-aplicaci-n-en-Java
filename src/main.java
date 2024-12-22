5b3d58cb733fad36121f01df45d412c3212051ce
Ferripro321 <150371738+XLopicX@users.noreply.github.com>
2024-12-22 16:23:58 +0100

    Agregada la capcitat de generar matriu aleatoria i mostrar-la

--- src/main.java
+++ src/main.java
@@ -1,5 +1,35 @@
+import java.util.Random;
+
 public class main {
+	static int matriu[][] = new int[5][5];
+	
 	public static void main(String[] args) {
-		
+		omplirMatriu();
+        mostrarTaula();
 	}
+	
+	public static void omplirMatriu() {
+        for (int i = 0; i < matriu.length; i++) {
+            for (int e = 0; e < matriu[i].length; e++) {
+                Random rand  = new Random();
+                matriu[i][e] = rand.nextInt(100); // per a que no surtin numeros fumada
+            }
+        }
+    }
+
+    public static void mostrarTaula() {
+        System.out.print("\n");
+        for (int i = 0; i < matriu.length; i++) {
+            for (int e = 0; e < matriu[i].length; e++) {
+                if (matriu[i][e] < 10) {
+                    System.out.print(matriu[i][e] + "  ");
+                }
+                else {
+                    System.out.print(matriu[i][e] + " ");
+                }
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
 }
